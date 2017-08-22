2c281e9ce1acabee4e2a0607ec448ef890c6d628
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2017-08-22 14:06:09 -0600

    Added Final Destination2 Restrooms

--- aws_iot/lamda_function.py
+++ aws_iot/lamda_function.py
@@ -56,7 +56,8 @@ hipchat = "https://changeagents.hipchat.com/v2/room/4036142/notification?auth_to
 slack = 'https://hooks.slack.com/services/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'
 loogoUrl = 'http://52.25.29.73:5002/washrooms/'
 echoPathUrl = 'http://52.25.29.73:5002/echopath/washrooms/'
-washrooms = {'G030MD037206HRE4': "RushHour2Mens", 'G030MD0232733SN8': "RushHour2Womens"}
+washrooms = {'G030MD037206HRE4': "RushHour2Mens", 'G030MD0232733SN8': "RushHour2Womens",
+             "G030MD039197T47D": "FinalDestination2Mens", "G030MD037225H5WR": "FinalDestination2Womens"}
 
 
 def handle_notifications(status_msg, battery_voltage, serial_no, color):
